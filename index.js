3cb826b82dc9bf6e2ed484d566ab91ad7b597dc0
Kashyap Patel <kashyappatel326@gmail.com>
2026-07-20 20:21:09 +0530

    something changed

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
-let scoreHome = document.getElementById("score-home");
+let scoreHome = document.getElementById("score-home")
 // console.log(scoreHome)
-let scoreAway = document.getElementById("score-away");
-// console.log(scoreAway)
-let count = 0;
-function homeAdd1() {
-  count += 1;
-  scoreHome.textContent = count;
-  let finalScore = scoreHome.textContent;
-  //   console.log(finalScore)
+let scoreGuest = document.getElementById("score-guest")
+// console.log(scoreGuest)
+ let count = 0
+function homeAdd1(){
+  count += 1
+  scoreHome.textContent = count
+  let finalScore = scoreHome.textContent
+//   console.log(finalScore)
 }
-function homeAdd2() {
-  count += 2;
-  scoreHome.textContent = count;
-  let finalScore = scoreHome.textContent;
-  //   console.log(finalScore)
+function homeAdd2(){
+  count += 2
+  scoreHome.textContent = count
+  let finalScore = scoreHome.textContent
+//   console.log(finalScore)
 }
-function homeAdd3() {
-  count += 3;
-  scoreHome.textContent = count;
-  let finalScore = scoreHome.textContent;
-  //   console.log(finalScore)
-}
-
-function awayAdd1() {
-  count += 1;
-  scoreAway.textContent = count;
-  let finalScore = scoreAway.textContent;
-  //   console.log(finalScore)
-}
-function awayAdd2() {
-  count += 2;
-  scoreAway.textContent = count;
-  let finalScore = scoreAway.textContent;
-  //   console.log(finalScore)
-}
-function awayAdd3() {
-  count += 3;
-  scoreAway.textContent = count;
-  let finalScore = scoreAway.textContent;
-  //   console.log(finalScore)
+function homeAdd3(){
+  count += 3
+  scoreHome.textContent = count
+  let finalScore = scoreHome.textContent
+//   console.log(finalScore)
 }
 
-function resetButton() {
-  count = 0;
-  scoreHome.textContent = count;
-  scoreAway.textContent = count;
+function guestAdd1(){
+  count += 1
+  scoreGuest.textContent = count
+  let finalScore = scoreGuest.textContent
+//   console.log(finalScore)
+}
+function guestAdd2(){
+  count += 2
+  scoreGuest.textContent = count
+  let finalScore = scoreGuest.textContent
+//   console.log(finalScore)
+}
+function guestAdd3(){
+  count += 3
+  scoreGuest.textContent = count
+  let finalScore = scoreGuest.textContent
+//   console.log(finalScore)
+}
+
+function resetButton(){
+    
+    count = 0
+    scoreHome.textContent = count
+    scoreGuest.textContent = count
 }
